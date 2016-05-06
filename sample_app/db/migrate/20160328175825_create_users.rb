4c58cb01999a6f029bd3ce9f1d88ebc2b91349ac
kishila <s16g459@stu.kagawa-u.ac.jp>
2016-05-06 05:01:29 -0700

    リスト6.33後のコマンドまで

--- sample_app/db/migrate/20160328175825_create_users.rb
+++ sample_app/db/migrate/20160328175825_create_users.rb
@@ -1,10 +1,7 @@
 class CreateUsers < ActiveRecord::Migration
   def change
-    create_table :users do |t|
-      t.string :name
-      t.string :email
-
-      t.timestamps
+    def change
+      add_index :users, :email, unique: true
     end
   end
 end
